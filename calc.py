93678fa32b5fd3cea38ac1304d3bfac6872825ca
nosorrow <plamenorama@gmail.com>
2025-06-20 12:30:53 +0300

    add feature

--- calc.py
+++ calc.py
@@ -3,3 +3,11 @@ def add(a, b):
 
 def subtract(a, b):
     return a - b
+
+
+print("This is a calculator module.")
+if __name__ == "__main__":
+    print("Running the calculator module directly.")
+else:
+    print("Importing the calculator module.")
+    print("Addition:", add(5, 3))
